8ea65fdcefcfdd2a1ed257e43c1a2441da701082
Renzo-Bravo <renzobravogomez@gmail.com>
2025-08-04 20:50:55 -0300

    fix bin

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,4 @@
 var express = require('express');
-<<<<<<< HEAD
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-=======
 const usersCtrl = require('../controllers/usersCtrl');
 var router = express.Router();
 
@@ -15,6 +7,5 @@ router.get('/login', usersCtrl.login);
 
 /* GET register page */
 router.get('/register', usersCtrl.register);
->>>>>>> temporaly
 
 module.exports = router;
