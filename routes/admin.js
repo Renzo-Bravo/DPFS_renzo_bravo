3708f997fd312dd34bb511e1d2d31dfa3b57e2fc
Renzo-Bravo <renzobravogomez@gmail.com>
2025-09-15 16:55:16 -0300

    form for create new categories and colors in function

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,10 @@
-var express = require('express');
-const adminCtrl = require('../controllers/adminCtrl');
+var express = require("express");
+const adminCtrl = require("../controllers/adminCtrl");
 var router = express.Router();
 
-router.get('/utils', adminCtrl.utils);
+router.get("/utils", adminCtrl.utils);
+
+router.post("/new-color", adminCtrl.createColor);
+router.post("/new-category", adminCtrl.createCategory);
 
 module.exports = router;
