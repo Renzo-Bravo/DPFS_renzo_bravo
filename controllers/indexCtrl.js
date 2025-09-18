8b69000ae0bbc5204f6e11eed49c3d3270b0b13e
Renzo-Bravo <renzobravogomez@gmail.com>
2025-09-17 23:57:36 -0300

    fix image brands in home

--- controllers/indexCtrl.js
+++ controllers/indexCtrl.js
@@ -6,9 +6,9 @@ const brandPath = path.join(__dirname, "../", "data", "brand.json");
 const indexCtrl = {
   index: function (req, res, next) {
     const brands = JSON.parse(fs.readFileSync(brandPath, "utf-8"));
-    console.log(brands + "holaa");
     res.render("index", { brands });
   },
 };
 
 module.exports = indexCtrl;
+
