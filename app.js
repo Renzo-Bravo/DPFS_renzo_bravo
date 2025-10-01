b4bb607120f817d03b150fe04cf5a783a57ac2ff
Renzo-Bravo <renzobravogomez@gmail.com>
2025-09-30 23:31:36 -0300

    isntallbcryptjs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var adminRouter = require('./routes/admin');
 var indexRouter = require('./routes/index');
