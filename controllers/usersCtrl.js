8e32aca6192589d14bcafcf5bb27203590850d7c
Renzo-Bravo <renzobravogomez@gmail.com>
2025-10-02 01:28:10 -0300

    add bcrypt and create users with form register (test with form login)

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 const fs = require("fs");
 const path = require("path");
 
@@ -12,17 +14,43 @@ const usersCtrl = {
   },
   profile: function (req, res, next) {
     const users = JSON.parse(fs.readFileSync(usersPath, "utf-8"));
-    console.log(users[0].name, "holaaaa")
+    console.log(users[0].name, "holaaaa");
     res.render("users/profile", { users });
   },
   processLogin: (req, res) => {
-    res.redirect("/")
+    const users = JSON.parse(fs.readFileSync(usersPath, "utf-8"));
+    const userFound = users.find((user) => user.email == req.body.email);
+    if (userFound) {
+      const passOk = bcrypt.compareSync(req.body.password, userFound.password);
+      if (passOk) {
+        res.render("USUARIO ENCONTRADO");
+      }
+      else{
+        res.render("NO ENCONTRADO")
+      }
+    } 
   },
   processRegister: (req, res) => {
-    console.log(req.file)
-    console.log(req.body)
-    res.redirect("/products/list")
-  }
+    const users = JSON.parse(fs.readFileSync(usersPath, "utf-8"));
+    const newUser = {
+      id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
+      checkbox: req.body.checkbox,
+      date: req.body.date,
+      email: req.body.email,
+      gender: req.body.gender,
+      name: req.body.name,
+      rol: req.body.rol,
+      surname: req.body.surname,
+      password: bcrypt.hashSync(req.body.password, 10),
+      image: req.file && req.file.filename ? req.file.filename : "profile.jpg",
+    };
+    console.log(newUser);
+
+    users.push(newUser);
+    const usersJSON = JSON.stringify(users, null, " ");
+    fs.writeFileSync(usersPath, usersJSON);
+    res.redirect("/");
+  },
 };
 
 module.exports = usersCtrl;
