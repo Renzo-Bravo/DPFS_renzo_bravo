d48a6d44373c036fa2c9224c6b5291fcd477e6e5
Renzo-Bravo <renzobravogomez@gmail.com>
2025-10-21 23:55:15 -0300

    Fix FK

--- database/models/Product.js
+++ database/models/Product.js
@@ -7,11 +7,7 @@ module.exports = (sequelize, DataTypes) => {
       autoIncrement: true,
       type: DataTypes.INTEGER.UNSIGNED,
     },
-<<<<<<< HEAD
     brand_id: {
-=======
-    brand: {
->>>>>>> 44c6d4c46823914186f0cf1c3b9888ea326ee203
       type: DataTypes.INTEGER.UNSIGNED,
       allowNull: false,
       references: {
@@ -45,11 +41,7 @@ module.exports = (sequelize, DataTypes) => {
     size_id: {
       type: DataTypes.INTEGER.UNSIGNED,
       references: {
-<<<<<<< HEAD
         model: "sizes",
-=======
-        model: "size",
->>>>>>> 44c6d4c46823914186f0cf1c3b9888ea326ee203
         key: "id",
       },
     },
@@ -71,45 +63,25 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
       references: {
-<<<<<<< HEAD
         model: "sales",
-=======
-        model: "sale",
->>>>>>> 44c6d4c46823914186f0cf1c3b9888ea326ee203
         key: "id",
       },
     },
   };
 
   const config = {
-<<<<<<< HEAD
     tableName: "products",
-=======
-    tableName: "Products",
->>>>>>> 44c6d4c46823914186f0cf1c3b9888ea326ee203
     timestamps: false,
   };
 
   const Product = sequelize.define(alias, cols, config);
 
-<<<<<<< HEAD
   Product.associate = (models) => {
     Product.belongsTo(models.Brand, { foreignKey: "brand_id", as: "marca" });
     Product.belongsTo(models.Category, { foreignKey: "category_id", as: "categoria" });
     Product.belongsTo(models.Colors, { foreignKey: "color_id", as: "colores" });
     Product.belongsTo(models.Genders, { foreignKey: "gender_id", as: "genero" });
     Product.belongsTo(models.Sale, { foreignKey: "sale_id", as: "oferta" });
-=======
-
-  //Los alias por ahora en español 
-
-  Product.associate = (models) => {
-    Product.belongsTo(models.Brand, { foreignKey: "brand_id", as: "marca" });
-    Product.belongsTo(models.Category, { foreignKey: "category_id", as: "categoria" });
-    Product.belongsTo(models.Color, { foreignKey: "color_id", as: "colores" });
-    Product.belongsTo(models.Gender, { foreignKey: "gender_id", as: "genero" });
-    Product.belongsTo(models.Sale, {foreignKey: "sale_id", as: "oferta"})
->>>>>>> 44c6d4c46823914186f0cf1c3b9888ea326ee203
     Product.belongsTo(models.Size, { foreignKey: "size_id", as: "talle" });
   };
 
