f84af2b0e831035e88019ca8248b1a66b126c39f
Renzo-Bravo <renzobravogomez@gmail.com>
2025-11-20 07:49:51 -0300

    Preparing structure

--- middlewares/logged.js
+++
@@ -1,9 +0,0 @@
-function logged (req,res, next) {
-    if(req.session.userLogged){
-        return next ();
-    }
-    return res.redirect("/users/login");
-}
-
-module.exports = logged;
-
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-const indexCtrl = require("../controllers/indexCtrl");
-var router = express.Router();
-
-/* GET home page. */
-router.get("/", indexCtrl.index);
-module.exports = router;
