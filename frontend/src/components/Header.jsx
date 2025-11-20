1fcec27926bf2165482f3f95f55d29a47655e041
Renzo-Bravo <renzobravogomez@gmail.com>
2025-11-20 14:11:23 -0300

    Create components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './Header.css';
+import MoviGoLogo from '../assets/MoviGo.png';
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <div className="navbar">
+          <i className="bi bi-list"></i>
+        </div>
+
+        <div className="logo">
+          <img src={MoviGoLogo} alt="Logo MoviGo" />
+        </div>
+
+        <div className="test">
+          <div className="search">
+            <i className="bi bi-search"></i>
+          </div>
+          <div className="cart">
+            <i className="bi bi-cart"></i>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Main.css';
+
+export default function Main() {
+  return (
+    <div>
+      <h5>Hola Mundo</h5>
+    </div>
+  )
+}
